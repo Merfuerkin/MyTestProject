d10c55e8c285ff5ac5cae8598926806c1facb841
Merfuerkin <merfu.erkin@gmail.com>
2023-08-24 21:53:18 -0400

    My test project created and pushed

---
+++ src/MyPracticePackage/practice.java
@@ -0,0 +1,11 @@
+package MyPracticePackage;
+
+public class practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("practing");
+	}
+
+}
